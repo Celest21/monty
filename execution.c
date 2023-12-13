2007f082bff5fb0f7bf0ae34cfb7db8d77282ffd
Celest21 <zc.chetty@gmail.com>
2023-12-13 15:29:36 -0800

    task 3

--- execution.c
+++ execution.c
@@ -36,6 +36,7 @@ void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number)
 		{"push", op_push},
 		{"pall", op_pall},
 		{"pint", op_pint},
+		{"pop", op_pop},
 		{NULL, NULL}
 	};
 	int i;
